2b1897c1b51da24c6c6843c7555511b6aa51c227
Navdeep Sood <nsood98@gmail.com>
2021-10-13 23:45:48 -0500

    initial commit

---
+++ contracts/SVGNFT.sol
@@ -0,0 +1,10 @@
+// give the contract some SVG code
+// output an NFT URI with this SVG code
+// storing all NFT metadata on-chain
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+contract SVGNFT {
+
+}
